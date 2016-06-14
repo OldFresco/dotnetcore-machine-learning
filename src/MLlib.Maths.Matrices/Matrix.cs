cd5a65c49ea8033c1ada7dbcb2c6ef02737cbee2
Kojo Hinson <hinsonkojo@gmail.com>
2016-06-14 18:37:01 +0100

    Flesh out matrix class

--- src/MLlib.Maths.Matrices/Matrix.cs
+++ src/MLlib.Maths.Matrices/Matrix.cs
@@ -1,4 +1,6 @@
-﻿namespace MLlib.Maths.Matrices
+﻿using System;
+
+namespace MLlib.Maths.Matrices
 {
     // This project can output the Class library as a NuGet Package.
     // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
@@ -6,55 +8,91 @@
     {
         public Matrix(int rowLength, int columnLength)
         {
+            if (rowLength < 1 || columnLength < 1)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
             RowLength = rowLength;
             ColumnLength = columnLength;
-            MatrixElementArray = new int[rowLength, columnLength];
+            MatrixElementArray = new double[rowLength, columnLength];
             ZeroInit();
         }
 
 
         public void ZeroInit()
         {
-            for (var rowIndex = 0; rowIndex < RowLength + 1; rowIndex++)
+            for (var columnIndex = 0; columnIndex < ColumnLength + 1; columnIndex++)
             {
-                if (MatrixElementArray != null) MatrixElementArray[rowIndex, columnIndex] = 0;
+                for (var rowIndex = 0; rowIndex < RowLength + 1; rowIndex++)
+                {
+                    MatrixElementArray[rowIndex, columnIndex] = 0;
+                }
             }
         }
 
         public void IdentityInit()
         {
-            
+            for (var columnIndex = 0; columnIndex < ColumnLength + 1; columnIndex++)
+            {
+                for (var rowIndex = 0; rowIndex < RowLength + 1; rowIndex++)
+                {
+                    if (rowIndex == columnIndex)
+                    {
+                        MatrixElementArray[rowIndex,columnIndex] = 1;
+                    }
+                    else
+                    {
+                        MatrixElementArray[rowIndex, columnIndex] = 0;
+                    }
+                }
+            }
+
         }
         public void RandomInit()
         {
-            
+            var randomNumberGenerator = new Random();
+            var number = randomNumberGenerator.Next(-100,100)/100;
+
+            for (var columnIndex = 0; columnIndex < ColumnLength + 1; columnIndex++)
+            {
+                for (var rowIndex = 0; rowIndex < RowLength + 1; rowIndex++)
+                {
+                    MatrixElementArray[rowIndex, columnIndex] = number;
+                }
+            }
         }
 
         public void Transpose()
         {
-            
+            for (var columnIndex = 0; columnIndex < ColumnLength + 1; columnIndex++)
+            {
+                for (var rowIndex = 0; rowIndex < RowLength + 1; rowIndex++)
+                {
+                    MatrixElementArray[rowIndex, columnIndex] = MatrixElementArray[ columnIndex, rowIndex];
+                }
+            }
         }
 
 
-        public Matrix GetSubMatrix()
+        public Matrix GetSubMatrix(int rowRange, int columnRange)
         {
-            return new Matrix(1,1);
+            return new Matrix(rowRange,columnRange);
         }
 
-        public int GetValueAt(int row, int column)
+        public double GetValueAt(int row, int column)
         {
-            return 0;
+            return MatrixElementArray[row, column];
         }
 
-        public int SetValueAt(int row, int column)
+        public void SetValueAt(int row, int column, double value)
         {
-            return 0;
-        }            
+            MatrixElementArray[row, column] = value;
+        }
 
         public int RowLength { get; }
         public int ColumnLength { get; }
-
-        public int[,] MatrixElementArray { get; set; }
+        public double[,] MatrixElementArray { get; set; }
     }
 
 
