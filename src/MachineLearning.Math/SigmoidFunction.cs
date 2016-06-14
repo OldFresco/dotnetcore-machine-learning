c5d86a1c4a45b647b9ca6d374186bb06a8a36263
OldFresco <hinsonkojo@gmail.com>
2016-06-14 01:15:34 +0100

    add project files

---
+++ src/MachineLearning.Math/SigmoidFunction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MachineLearning.Math
+{
+    public static class SigmoidFunction
+    {
+        public static double Calc(double x)
+        {
+            return 1 / (1 + System.Math.Exp(-x));
+        }
+
+        public static double CalcDeriv(double x)
+        {
+            double s = Calc(x);
+            return s * (1 - s);
+        }
+    }
+}
